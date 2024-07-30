1e0ae41aa7c794828e069fa19accbe24a5ebb33b
AnatoleA <anatoleaze@hotmail.fr>
2024-07-30 18:54:10 +0200

    add login and register in menu

--- resources/js/components/contact/ContactFormComponent.vue
+++ resources/js/components/contact/ContactFormComponent.vue
@@ -15,7 +15,7 @@
     
 
         <div class="bor8 m-b-20 how-pos4-parent">
-            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="email" name="email" v-model="form.email" :value="userEmail"  required>
+            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="email" name="email" v-model="form.email" value="userEmail"  required>
             <img class="how-pos4 pointer-none" src="images/icons/icon-email.png" alt="ICON">
         </div>
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
 				<nav class="limiter-menu-desktop p-l-45">
 					
 					<!-- Logo desktop -->		
-					<a href="#" class="logo">
+					<a href="{{ url('/') }}" class="logo">
 						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
 					</a>
 
@@ -59,6 +59,8 @@
 						</ul>
 					</div>	
 
+					
+
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m h-full">
 						<div class="flex-c-m h-full p-r-24">
@@ -73,6 +75,60 @@
 							</div>
 						</div>
 							
+						<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+							<div class="container">
+								
+								<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+									<span class="navbar-toggler-icon"></span>
+								</button>
+				
+								<div class="collapse navbar-collapse" id="navbarSupportedContent">
+									<!-- Left Side Of Navbar -->
+									<ul class="navbar-nav mr-auto">
+									</ul>
+				
+									<!-- Right Side Of Navbar -->
+									<ul class="navbar-nav ml-auto">
+										<!-- Authentication Links -->
+										@guest
+											<li class="nav-item">
+												<a class="nav-link" href="{{ route('login') }}">Connexion</a>
+											</li>
+											@if (Route::has('register'))
+												<li class="nav-item">
+													<a class="nav-link" href="{{ route('register') }}">Inscription</a>
+												</li>
+											@endif
+										@else
+											<li class="nav-item dropdown">
+												<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+													{{ Auth::user()->name }} <img src="images/profil.png" style="width: 30px;" alt="IMG-PROFILs"><span class="caret"></span>
+												</a>
+				
+												<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+													<a class="dropdown-item" href="#">
+														Mon profil
+													</a>
+													<a class="dropdown-item" href="#">
+														Mes commandes
+													</a>
+													<a class="dropdown-item" href="{{ route('logout') }}"
+													   onclick="event.preventDefault();
+																	 document.getElementById('logout-form').submit();">
+														Déconnexion
+													</a>
+				
+													<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+														@csrf
+													</form>
+												</div>
+											</li>
+										@endguest
+									</ul>
+								</div>
+							</div>
+						</nav>
+					
 					</div>
 				</nav>
 			</div>	
